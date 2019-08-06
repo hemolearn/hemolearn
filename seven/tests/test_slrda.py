f7fbd3344a0a20a4ddf05b20c3c8511b4b4a10e6
Hamza CHERKAOUI <hamza.cherkao@gmail.com>
2019-08-06 16:53:52 +0200

    TEST test main model

---
+++ seven/tests/test_slrda.py
@@ -0,0 +1,20 @@
+"""Testing module for the utility functions"""
+# Authors: Hamza Cherkaoui <hamza.cherkaoui@inria.fr>
+# License: BSD (3-clause)
+
+import pytest
+from nilearn import datasets
+from seven import SLRDA
+
+
+def test_slrda():
+    """ Test the main class: check that no Exception is raised """
+    try:
+        X = datasets.fetch_adhd(n_subjects=1).func[0]
+        slrda = SLRDA(n_atoms=5, t_r=2.0, hrf_atlas='scale007',
+                      hrf_model='3_basis_hrf', lbda=5.0e-3, max_iter=2,
+                      eps=1.0-6, raise_on_increase=True, random_state=0,
+                      n_jobs=1, nb_fit_try=1, verbose=0)
+        slrda.fit(X)
+    except Exception as e:
+        pytest.fail("Unexpected Exception raised: {}".format(e))
