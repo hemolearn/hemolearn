b7fbd4a7c499495fd28e5d001469e621d89b4546
hcherkaoui <hamza.cherkaoui@polytechnique.edu>
2021-10-25 16:18:15 +0200

    EX polishing real data example

--- examples/02_real_fmri_data/plot_adhd_rest.py
+++ examples/02_real_fmri_data/plot_adhd_rest.py
@@ -19,9 +19,7 @@ import os
 from nilearn import datasets
 import numpy as np
 
-from hemolearn import SLRDA
-from hemolearn.plotting import (plot_spatial_maps, plot_temporal_activations,
-                                plot_vascular_map)
+from hemolearn import SLRDA, plotting
 
 
 ###############################################################################
@@ -34,9 +32,7 @@ if not os.path.exists(plot_dir):
 ###############################################################################
 # Fetch fMRI subjects
 # -------------------------
-seed = 0
-TR = 2.0
-n_subjects = 4
+seed, TR, n_subjects = 0, 2.0, 4
 adhd_dataset = datasets.fetch_adhd(n_subjects=n_subjects)
 func_fnames = adhd_dataset.func[:n_subjects]
 confound_fnames = adhd_dataset.confounds[:n_subjects]
@@ -44,47 +40,32 @@ confound_fnames = adhd_dataset.confounds[:n_subjects]
 ###############################################################################
 # Distangle the neurovascular coupling from the neural activation
 # ---------------------------------------------------------------
-slrda = SLRDA(n_atoms=10, t_r=TR, n_times_atom=20, lbda=0.75, max_iter=30,
-              eps=1.0e-3, shared_spatial_maps=True, random_state=seed,
-              verbose=2)
-slrda.fit(func_fnames, confound_fnames)
+slrda = SLRDA(n_atoms=10, t_r=TR, n_times_atom=30, lbda=0.1, max_iter=50,
+              standardize=True, shared_spatial_maps=True, random_state=seed,
+              verbose=1)
+a_hat_img = slrda.fit_transform(func_fnames, confound_fnames=confound_fnames)
 
 ###############################################################################
 # Plot the spatial maps
 # ---------------------
-if slrda.shared_spatial_maps or n_subjects == 1:
-    filename = os.path.join(plot_dir, f'spatial_maps.png')
-    plot_spatial_maps(slrda.u_hat_img, filename=filename,
-                      perc_voxels_to_retain='10%', verbose=True)
-else:
-    for n in range(n_subjects):
-        filename = os.path.join(plot_dir, f'spatial_maps_{n}.png')
-        plot_spatial_maps(slrda.u_hat_img[n], filename=filename,
-                          perc_voxels_to_retain='10%', verbose=True)
+filename = os.path.join(plot_dir, f'spatial_maps.png')
+plotting.plot_spatial_maps(slrda.u_hat_img, filename=filename,
+                           perc_voxels_to_retain='1%', verbose=True)
 
 ###############################################################################
 # Plot the temporal activations
 # -----------------------------
-if n_subjects == 1:
-    filename = os.path.join(plot_dir, f'activations.png')
-    plot_temporal_activations(slrda.z_hat, TR, filename=filename, verbose=True)
-else:
-    for n in range(n_subjects):
-        filename = os.path.join(plot_dir, f'activations_{n}.png')
-        plot_temporal_activations(slrda.z_hat[n], TR, filename=filename,
-                                  verbose=True)
+for n in range(n_subjects):
+    filename = os.path.join(plot_dir, f'activations_{n}.png')
+    plotting.plot_temporal_activations(slrda.z_hat[n], TR, filename=filename,
+                                       verbose=True)
 
 ###############################################################################
 # Plot vascular maps
 # ------------------
-if n_subjects == 1:
-    filename = os.path.join(plot_dir, f'vascular_maps.png')
-    plot_vascular_map(slrda.a_hat_img, display_mode='z',
-                      cut_coords=np.linspace(-30, 60, 5),
-                      filename=filename, verbose=True)
-else:
-    for n in range(n_subjects):
-        filename = os.path.join(plot_dir, f'vascular_maps_{n}.png')
-        plot_vascular_map(slrda.a_hat_img[n], display_mode='z',
-                          cut_coords=np.linspace(-30, 60, 5),
-                          filename=filename, verbose=True)
+vmax = np.max([np.max(slrda.a_hat[n]) for n in range(n_subjects)])
+for n in range(n_subjects):
+    filename = os.path.join(plot_dir, f'vascular_maps_{n}.png')
+    plotting.plot_vascular_map(a_hat_img[n], display_mode='z',
+                               cut_coords=np.linspace(-30, 60, 5),
+                               filename=filename, vmax=vmax, verbose=True)
